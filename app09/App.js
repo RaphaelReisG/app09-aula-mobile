4088994765a067f5c0402e58a78dcc1d848d422c
RaphaelReisG <86501038+RaphaelReisG@users.noreply.github.com>
2023-04-14 12:05:23 -0300

    remove br tag

--- app09/App.js
+++ app09/App.js
@@ -20,21 +20,21 @@ export default function App() {
           <Text style={styles.conteudo}> Salário: R$ 5000,00 </Text>
           <Text style={styles.conteudo}> Descrição: Atuar criando e analisando sistemas WEB em linguegem JAVA. </Text>
           <Text style={styles.conteudo}> Contato: curriculo@americanas.com </Text>
-          <br/>
+         
         </Card>
         <Card style={styles.cards}>
           <Text style={styles.titulo2}> Programador C# </Text>
           <Text style={styles.conteudo}> Salário: R$ 3000,00 </Text>
           <Text style={styles.conteudo}> Descrição: Atuar programando em sistemas existentes e desenvolvendo novas funcionalidades em C#. </Text>
           <Text style={styles.conteudo}> Contato: curriculo@microsoft.com </Text>
-          <br/>
+          
         </Card>
         <Card style={styles.cards}>
           <Text style={styles.titulo2}> Analista JR </Text>
           <Text style={styles.conteudo}> Salário: R$ 4000,00 </Text>
           <Text style={styles.conteudo}> Descrição: Atuar com analise de negocio do cliente, analise de requisitos, realizar diagramas da UML e outros. </Text>
           <Text style={styles.conteudo}> Contato: curriculo@google.com </Text>
-          <br/>
+          
         </Card>
       </ScrollView>
     </View>
